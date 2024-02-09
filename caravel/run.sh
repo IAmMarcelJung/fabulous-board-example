6f54802b81276ce17658374b70d531232c55bd2c
IAmMarcelJung <MarcelJung97@web.de>
2024-02-09 18:42:15 +0100

    add adjusted scripts to upload a bistream

--- caravel/run.sh
+++ caravel/run.sh
@@ -2,7 +2,7 @@
 set -ex
 
 mpy-cross caravel_spi.py
-cp caravel_spi.mpy ${FLASH}
-cp ../sim/test_design/bram32.bin ${FLASH}/counter.bin
+rshell cp caravel_spi.mpy ${FLASH}
+rshell cp ../sim/test_design/${DESIGN}.bin ${FLASH}/counter.bin
 sync
 mpremote connect ${DEV} exec "import caravel_spi; caravel_spi.run()"
--- caravel/run_demo_intial.sh
+++ caravel/run_demo_intial.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-rshell cp ./demo/counter.bin ${FLASH}
+rshell cp ./demo/* ${FLASH}
 sync
 mpremote connect ${DEV} exec "import caravel_spi_vga_bram; caravel_spi_vga_bram.run()"
