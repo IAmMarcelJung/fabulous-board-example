21989b9e0448fbe9d945c8c8933df5059a4f2fcf
IAmMarcelJung <MarcelJung97@web.de>
2024-02-21 12:38:42 +0100

    refactor: format using black and add sleep after uploading the bitstream

--- caravel/caravel_spi.py
+++ caravel/caravel_spi.py
@@ -5,22 +5,18 @@ import time
 from machine import Pin
 from modules.board import Board
 
+
 def run():
     Board.set_voltage(1.8)
 
-    fpga_clk = Pin('IO_7', mode=Pin.OUT, value=0)
-    fpga_clksel0 = Pin('IO_8', mode=Pin.OUT, value=0)
-    fpga_clksel1 = Pin('IO_9', mode=Pin.OUT, value=0)
-    fpga_sclk = Pin('IO_10', mode=Pin.OUT, value=0)
-    fpga_sdata = Pin('IO_11', mode=Pin.OUT, value=0)
-    fpga_rst = Pin('IO_14', mode=Pin.OUT, value=0)
+    fpga_clk = Pin("IO_7", mode=Pin.OUT, value=0)
+    fpga_clksel0 = Pin("IO_8", mode=Pin.OUT, value=0)
+    fpga_clksel1 = Pin("IO_9", mode=Pin.OUT, value=0)
+    fpga_sclk = Pin("IO_10", mode=Pin.OUT, value=0)
+    fpga_sdata = Pin("IO_11", mode=Pin.OUT, value=0)
+    fpga_rst = Pin("IO_14", mode=Pin.OUT, value=0)
 
-    board = Board(fpga_clk,
-                fpga_clksel0,
-                fpga_clksel1,
-                fpga_sclk,
-                fpga_sdata,
-                fpga_rst)
+    board = Board(fpga_clk, fpga_clksel0, fpga_clksel1, fpga_sclk, fpga_sdata, fpga_rst)
 
     board.startup_sequence()
 
@@ -32,9 +28,11 @@ def run():
     # use slow clock for running
     board.set_slow_clock()
 
+    print("Sleep for five seconds.")
+    time.sleep(5)
     board.print_fpga_data()
 
-    '''
+    """
     used_pins =  [16, 17, 18, 19, 22, 23, 24]
     pin_pos = []
     #for pin in used_pins:
@@ -54,14 +52,14 @@ def run():
             #cnt_value += f"{pin}: {fpga_data[pin].value()}, "
         cnt_value += "\n"
         print(cnt_value)
-    '''
-    '''
+    """
+    """
         b = 0
         for k, p in enumerate(fpga_data):
             if p.value():
                 b |= (1 << k)
         print("data: {:023b}".format(b))
-    '''
+    """
 
     #    time.sleep(0.01)
     #    fpga_clk.value(1)
