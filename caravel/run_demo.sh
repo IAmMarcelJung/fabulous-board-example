1f007573f916500cdc87e410967b6fb892466031
IAmMarcelJung <MarcelJung97@web.de>
2024-03-26 09:57:47 +0100

    formatting

--- caravel/run_demo.sh
+++ caravel/run_demo.sh
@@ -5,13 +5,13 @@ mpy-cross caravel_spi_vga_bram.py
 mpy-cross ./modules/board.py
 mpy-cross ./modules/myspi.py
 
-
 rshell cp caravel_spi_vga_bram.mpy ${FLASH}
 
 rshell mkdir  ${FLASH}/modules
+
 rshell cp ./modules/myspi.mpy ${FLASH}/modules/
 rshell cp ./modules/board.mpy ${FLASH}/modules/
-
 rshell cp ./demo/counter.bin ${FLASH}
+
 sync
 mpremote connect ${DEV} exec "import caravel_spi_vga_bram; caravel_spi_vga_bram.run()"
