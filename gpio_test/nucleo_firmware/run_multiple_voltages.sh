b47b39843757a244eeb9768c86e59f78dc15a05c
IAmMarcelJung <MarcelJung97@web.de>
2024-03-27 11:36:06 +0100

    fix: add check for existing directory and create it if it does not exist

--- gpio_test/nucleo_firmware/run_multiple_voltages.sh
+++ gpio_test/nucleo_firmware/run_multiple_voltages.sh
@@ -3,10 +3,14 @@
 SELECTED_PART=$1;
 voltages=("1.45" "1.6" "1.8");
 part_str="part_$SELECTED_PART"
+CONFIG_FILE_DIR=./gpio_config_files
 if [ -z "$SELECTED_PART" ]; then 
     echo "Please specify a part number."
-
 else
+    if [ ! -d "$part_str" ]; then 
+        echo "$CONFIG_FILE_DIR/$part_str does not exist. Creating it."
+        mkdir "$CONFIG_FILE_DIR/$part_str"
+    fi
     for voltage in "${voltages[@]}"; do
         echo "Starting run for $voltage"
         make run PART=$SELECTED_PART VOLTAGE=$voltage
