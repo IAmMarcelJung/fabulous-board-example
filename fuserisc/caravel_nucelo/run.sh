e47fc8a9e70ebb28b704d501e933356796ff6476
IAmMarcelJung <MarcelJung97@web.de>
2024-04-02 11:30:40 +0200

    delete fuserisc directory

--- fuserisc/caravel_nucelo/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-mpy-cross caravel_spi.py
-cp caravel_spi.mpy ${FLASH}
-cp ../test_designs/counter.bin ${FLASH}/counter.bin
-sync
-mpremote connect ${DEV} exec "import caravel_spi; caravel_spi.run()"
--- fuserisc/sim/tests/run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-iverilog -o $1.vvp -s testbench $1 ../rtl/*.v
-vvp $1.vvp -fst
