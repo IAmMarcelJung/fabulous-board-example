17187bd7a70d149413bd6a0f9a3fd8923b496899
IAmMarcelJung <MarcelJung97@web.de>
2024-04-02 13:26:11 +0200

    add endif

--- sim/run_simulation.sh
+++ sim/run_simulation.sh
@@ -4,7 +4,9 @@ DESIGN=counter
 BITSTREAM=test_design/${DESIGN}/${DESIGN}.bin
 VERILOG=../fabric/verilog
 MAX_BITBYTES=16384
+USER_DESIGN=../user_design/
+OUTPUT=../output_files
 
-iverilog -s fab_tb -o fab_tb.vvp $VERILOG/*.v test_design/${DESIGN}/${DESIGN}.v fabulous_tb.v models_pack.v ${IVFLAGS} -DCREATE_VCD
-python3 makehex.py $BITSTREAM $MAX_BITBYTES bitstream.hex
+iverilog -s fab_tb -o ${OUTPUT}/fab_tb.vvp $VERILOG/*.v ${USER_DESIGN}/${DESIGN}/${DESIGN}.v fabulous_tb.v models_pack.v ${IVFLAGS} -DCREATE_VCD
+python3 makehex.py $BITSTREAM $MAX_BITBYTES ${OUTPUT}/bitstream.hex
 vvp fab_tb.vvp -fst
