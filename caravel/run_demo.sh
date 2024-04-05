c1ee52717d526d29081a7e046ddcf89dd184dfaf
IAmMarcelJung <MarcelJung97@web.de>
2024-04-05 18:04:26 +0200

    adjust script to copy all files to the board and run the demo

--- caravel/run_demo.sh
+++ caravel/run_demo.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
+
+DEV=$(mpremote connect list | grep STLink | cut -f 1 -d ' ')
+RSHELL_PORT="rshell --port ${DEV}"
+COPY_CMD="${RSHELL_PORT} cp"
+MKDIR_CMD="${RSHELL_PORT} mkdir"
+
 set -ex
 
-mpy-cross caravel_spi_vga_bram.py
+mpy-cross fabulous_demo.py
 mpy-cross ./modules/board.py
 mpy-cross ./modules/myspi.py
 
-rshell cp caravel_spi_vga_bram.mpy ${FLASH}
+${COPY_CMD} fabulous_demo.mpy ${FLASH}
 
-rshell mkdir  ${FLASH}/modules
+${MKDIR_CMD} ${FLASH}/modules
 
-rshell cp ./modules/myspi.mpy ${FLASH}/modules/
-rshell cp ./modules/board.mpy ${FLASH}/modules/
-rshell cp ./demo/counter.bin ${FLASH}
+${COPY_CMD} ./modules/myspi.mpy ${FLASH}/modules/
+${COPY_CMD} ./modules/board.mpy ${FLASH}/modules/
+${COPY_CMD} ./../output_files/bitstream.bin ${FLASH}
+${COPY_CMD} ./street.bin ${FLASH}
 
 sync
-mpremote connect ${DEV} exec "import caravel_spi_vga_bram; caravel_spi_vga_bram.run()"
+mpremote connect ${DEV} exec "import fabulous_demo; fabulous_demo.run()"
