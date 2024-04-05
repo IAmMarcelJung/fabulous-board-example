ebfdba9982ccdf42d01f8924cfd7fb07e93b0701
IAmMarcelJung <MarcelJung97@web.de>
2024-04-05 18:57:58 +0200

    doc: add comment about why the 15 second sleep is needed

--- caravel/main.py
+++ caravel/main.py
@@ -3,9 +3,8 @@ import io_config
 import time
 import fabulous_demo
 
-# sw = pyb.Switch()
-# sw.callback(io_config.run())
-# io_config.run()
 io_config.run_poweron()
+
+# This is needed to make sure the GPIO configuration is done.
 time.sleep(15)
 fabulous_demo.run()
