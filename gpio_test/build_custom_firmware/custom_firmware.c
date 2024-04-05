720885ea96e6a17e6f40a4e7a0333838f2fe078a
IAmMarcelJung <MarcelJung97@web.de>
2024-04-05 17:36:38 +0200

    remove unnused code, fix includes, use correct functions

--- gpio_test/build_custom_firmware/custom_firmware.c
+++ gpio_test/build_custom_firmware/custom_firmware.c
@@ -1,54 +1,9 @@
-//#include "../calibration/runtime.h"
 #include "../riscv_firmware_src/defs.h"
-#include "../gpio_config/gpio_config_io.c"
+#include "../gpio_config/gpio_config_io.h"
 
 // --------------------------------------------------------
 // Firmware routines
 // --------------------------------------------------------
-
-void set_registers() {
-    reg_mprj_io_0 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_1 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_2 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_3 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_4 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_5 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_6 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_7 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_8 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_9 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_10 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_11 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_12 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_13 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_14 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_15 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_16 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_17 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_18 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-
-    reg_mprj_io_19 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_20 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_21 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_22 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_23 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_24 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_25 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_26 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_27 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_28 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_29 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_30 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_31 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_32 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_33 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_34 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_35 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_36 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-    reg_mprj_io_37 = GPIO_MODE_USER_STD_BIDIRECTIONAL;
-
-}
-
 
 int main() {
     reg_gpio_mode1 = 1;
@@ -58,8 +13,6 @@ int main() {
     reg_gpio_out = 1;
 
     gpio_config_io();
-
-    set_registers();
 
     reg_gpio_out = 0;
 }
