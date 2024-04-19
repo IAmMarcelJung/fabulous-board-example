fdcb2b87b27516796ead4a40b3d8b18705676396
IAmMarcelJung <MarcelJung97@web.de>
2024-04-19 17:34:51 +0200

    fix: use a macro defitinon instead of the wrong sizeof(uint8_t) for the delimiter in the for loop, also add u to the control word definitions

--- gpio_test/firmware_helpers/upload_bitstream/upload_bitstream.c
+++ gpio_test/firmware_helpers/upload_bitstream/upload_bitstream.c
@@ -3,8 +3,10 @@
 #define PIN_SCLK IO_10_DATA_BIT_POS
 #define PIN_SDATA IO_11_DATA_BIT_POS
 
-#define CTRL_WORD_DISABLE_BITBANG 0x0000FAB0
-#define CTRL_WORD_ENABLE_BITBANG 0x0000FAB1
+#define CTRL_WORD_DISABLE_BITBANG 0x0000FAB0u
+#define CTRL_WORD_ENABLE_BITBANG 0x0000FAB1u
+
+#define BITS_IN_BYTE 8u
 
 /**
  * @brief Use bitbanging to upload the bitstream to the FPGA.
@@ -17,7 +19,7 @@ void upload_bitstream(uint8_t const *const bitstream_data,
         uint32_t bitream_size)
 {
     for (uint32_t byte_pos = 0u; byte_pos < bitream_size; byte_pos++) {
-        for (uint32_t bit_pos = 0u; bit_pos < sizeof(uint8_t); bit_pos++) {
+        for (uint32_t bit_pos = 0u; bit_pos < BITS_IN_BYTE; bit_pos++) {
             bool set;
             set = (bool)(bitstream_data[byte_pos] >> (7u - bit_pos) & 0x1u);
             set_or_clear_pin(PIN_SDATA, LOW_CHAIN, set);
