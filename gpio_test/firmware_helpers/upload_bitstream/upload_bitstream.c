6045f358a7c416c01f32efdb2c4b8777a8ceb1f4
IAmMarcelJung <MarcelJung97@web.de>
2024-04-23 09:54:20 +0200

    refactor: use the global defintions in the upload_bistream module

--- gpio_test/firmware_helpers/upload_bitstream/upload_bitstream.c
+++ gpio_test/firmware_helpers/upload_bitstream/upload_bitstream.c
@@ -1,3 +1,4 @@
+#include <global_defs.h>
 #include <upload_bitstream.h>
 
 #define PIN_SCLK GPIO_10
@@ -5,8 +6,6 @@
 
 #define CTRL_WORD_DISABLE_BITBANG 0x0000FAB0u
 #define CTRL_WORD_ENABLE_BITBANG 0x0000FAB1u
-
-#define BITS_IN_BYTE 8u
 
 /**
  * @brief Use bitbanging to upload the bitstream to the FPGA.
@@ -21,14 +20,14 @@ void upload_bitstream(uint8_t const *const bitstream_data,
     for (uint32_t byte_pos = 0u; byte_pos < bitream_size; byte_pos++) {
         for (uint32_t bit_pos = 0u; bit_pos < BITS_IN_BYTE; bit_pos++) {
             bool set;
-            set = (bool)(bitstream_data[byte_pos] >> (7u - bit_pos) & 0x1u);
-            set_or_clear_pin(PIN_SDATA, LOW_CHAIN, set);
-            set_pin(PIN_SCLK, LOW_CHAIN);
+            set = (bool)(bitstream_data[byte_pos] >> (MSB_IN_BYTE - bit_pos) & 1u);
+            set_or_clear_pin(PIN_SDATA, set);
+            set_pin(PIN_SCLK);
             set = (bool)(CTRL_WORD_ENABLE_BITBANG >>
-                    (31u - (8u * (byte_pos % 4u) + bit_pos)) &
-                    0x1u);
-            set_or_clear_pin(PIN_SDATA, LOW_CHAIN, set);
-            clear_pin(PIN_SCLK, LOW_CHAIN);
+                    (MSB_IN_WORD - (BYTES_IN_WORD * (byte_pos % BYTES_IN_WORD) + bit_pos)) &
+                    1u);
+            set_or_clear_pin(PIN_SDATA, set);
+            clear_pin(PIN_SCLK);
         }
     }
 }
