5b02f6d9c06592770461fdb6494a553e299e2b77
IAmMarcelJung <MarcelJung97@web.de>
2024-10-15 10:25:19 +0200

    feat: add separate files for helper functions

---
+++ gpio_test/firmware_helpers/helpers/helpers.h
@@ -0,0 +1,10 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <defs.h>
+#include <global_defs.h>
+
+void blink(uint32_t toggles, uint32_t interval);
+void print_word(uint32_t word);
+
+#endif /* HELPERS_H */
