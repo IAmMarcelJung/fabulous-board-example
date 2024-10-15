044eaf500d25e6ab998d27f3347ffbb9592e8030
IAmMarcelJung <MarcelJung97@web.de>
2024-10-15 10:31:04 +0200

    feat: adjust the IO width to the actual IO width, move reset to IO[37], use the prescaler to reduce the counter frequency

--- user_design/counter/counter.v
+++ user_design/counter/counter.v
@@ -1,29 +1,32 @@
-module top(input wire clk, input wire [23:0] io_in, output wire [23:0] io_out, io_oeb);
-wire rst = io_in[0];
-reg [23:0] ctr;
-reg [15:0] prescale;
-localparam PRESCALE_LIMIT = 1000;
+module top (
+    input wire clk,
+    input wire [23:0] io_in,
+    output wire [23:0] io_out,
+    io_oeb
+);
+    wire rst;
 
-always @(posedge clk) begin
-    if (rst)
-        ctr <= 0;
-    else begin
-        //if (prescale == PRESCALE_LIMIT)
-        ctr <= ctr + 1'b1;
+    reg [23:0] ctr;
+    reg [15:0] prescale;
+    localparam PRESCALE_LIMIT = 6;
+
+    always @(posedge clk) begin
+        if (rst) ctr <= 0;
+        else begin
+            if (prescale == PRESCALE_LIMIT) ctr <= ctr + 1'b1;
+        end
     end
-end
 
-always @(posedge clk) begin
-    if (rst)
-        prescale <= 0;
-    else begin
-        if (prescale == PRESCALE_LIMIT)
-            prescale <= 0;
-        else
-            prescale <= prescale + 1'b1;
+    always @(posedge clk) begin
+        if (rst) prescale <= 0;
+        else begin
+            if (prescale == PRESCALE_LIMIT) prescale <= 0;
+            else prescale <= prescale + 1'b1;
+        end
     end
-end
 
-assign io_out[23:1] = ctr[23:1]; // pass thru reset for debugging
-assign io_oeb = ~(24'b1);
+    assign io_out[21:0] = ctr[21:0];
+    assign io_out[22] = rst;
+    assign rst = io_in[23];
+    assign io_oeb = ~(24'b100000000000000000000000);
 endmodule
