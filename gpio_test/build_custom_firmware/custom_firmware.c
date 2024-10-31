e47b608f6404e23ba23b5fda5867d5b59a10538d
IAmMarcelJung <MarcelJung97@web.de>
2024-10-31 15:49:04 +0100

    misc: remove unnecessary code from custom_firmware.c

--- gpio_test/build_custom_firmware/custom_firmware.c
+++ gpio_test/build_custom_firmware/custom_firmware.c
@@ -1,10 +1,6 @@
 #include "../gpio_config/gpio_config_io.h"
 #include <bitstream.h>
-#include <defs.h>
-#include <global_defs.h>
 #include <helpers.h>
-#include <register_actions.h>
-#include <stub.h>
 #include <upload_bitstream.h>
 
 int main() {
@@ -13,7 +9,6 @@ int main() {
     reg_gpio_ien = 1;
     reg_gpio_oe = 1;
     reg_gpio_out = 1;
-    reg_uart_enable = 1u;
 
     blink(3, 5000000);
     reg_gpio_out = 1;
