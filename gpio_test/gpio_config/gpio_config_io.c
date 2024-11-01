326512050f823ec856dd6fab504323913674e6b1
IAmMarcelJung <MarcelJung97@web.de>
2024-11-01 16:45:49 +0100

    remove call of clear_registers() in gpio_config_io to reduce the GPIO configuration time, since it is not needed

--- gpio_test/gpio_config/gpio_config_io.c
+++ gpio_test/gpio_config/gpio_config_io.c
@@ -45,17 +45,13 @@ void clear_registers() {
 }
 
 void gpio_config_io() {
-    int i = 1; // start offset 1, first value is n_bits
     int n_bits = config_stream[0];
-    //    bb_mode();
-    clear_registers();
-    // int n_bits = sizeof(config_stream);
-    while (i < n_bits) {
+    // start at  offset 1, first value is n_bits
+    for (int i = 1u; i < n_bits + 1; i++) {
         reg_mprj_xfer = config_stream[i];
         delay(WAIT);
         reg_mprj_xfer = config_stream[i] + 0x10;
         delay(WAIT);
-        i++;
     }
     load();
 }
