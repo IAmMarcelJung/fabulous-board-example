955a573aa4a6cf17015c7a4d1531b285a35dd31f
IAmMarcelJung <MarcelJung97@web.de>
2024-11-01 16:32:34 +0100

    add comment and reduce blinking time in gpio_config_firmware.c

--- gpio_test/build_custom_firmware/gpio_config_firmware.c
+++ gpio_test/build_custom_firmware/gpio_config_firmware.c
@@ -13,14 +13,17 @@ int main() {
     reg_gpio_oe = 1;
     reg_gpio_out = 1;
 
-    blink(3, 5000000);
+    blink(3, 2500000);
+
+    // Configure the IOs so the eFPGA has access to them
     reg_gpio_out = 1;
     gpio_config_io();
 
-    blink(3, 5000000);
+    blink(3, 2500000);
     reg_gpio_out = 0;
-    while (1)
-        ;
+
+    while (1) {
+    }
 
     return 0;
 }
