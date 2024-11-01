ec8fa0c09b0b57354cbf36ddfd1a6d850394a86a
IAmMarcelJung <MarcelJung97@web.de>
2024-11-01 16:51:03 +0100

    refactor: complete refactor of gpio_config_builder.py, code is divided into functions, documentation was added, core functionality stays the same

--- gpio_test/gpio_config/gpio_config_builder.py
+++ gpio_test/gpio_config/gpio_config_builder.py
@@ -1,221 +1,302 @@
 #!/bin/env python3
-#
+
 # gpio_config_builder.py
-#          Build a pair of configuration bit streams for GPIO on MPW-2 accounting for
-#          hold violations between gpio blocks.
+#          Build a pair of configuration bit streams for GPIO on MPW-2 to MPW-5
+#          accounting for hold violations between gpio blocks.
 #
-# Input:   Hold violations between each GPIO and desired
+# Input:   Hold violations between each GPIO and desired configuration
 # Output:  Configuration bitsteams for upper and lower gpio chains
-#
 
-# import gpio and configuration definitions
-# from gpio_config_def import NUM_IO, C_MGMT_IN, C_MGMT_OUT, C_USER_BIDIR, C_DISABLE, C_ALL_ONES, \
-#                             H_DEPENDENT, H_INDEPENDENT, H_NONE, H_SPECIAL, config_h, config_l, gpio_h, gpio_l, \
-#                             C_USER_BIDIR_WPU, C_USER_BIDIR_WPD, C_USER_IN_NP, C_USER_OUT
-
-import os, sys
+import os
+import sys
+import datetime
+from typing import Tuple, List, TextIO
 
 sys.path.append(os.getcwd())
 
 from gpio_config_def import *
 from gpio_config_io import *
 
-# ------------------------------------------
+
+NUM_CONFIG_BITS = 13
+CONFIG_STREAM_TARGET_LEN = 247
 
 
-stream_h = ""
-stream_l = ""
-config_stream = []
+def add_config_to_stream(stream: str, config: int, htv_type: int) -> str:
+    """Add the intended config to the stream. Some preparations for correcting
+    the hold time violation pattern are also done here.
 
-
-def build_stream_dependent(stream, config):
-    s = ""
+    :param stream: The configuration stream where the config is added to.
+    :type stream: str
+    :param config: The intended config for the current pin.
+    :type config: int
+    :param htv_type: The type of hold time violation to consider.
+    :type htv_type: int
+    :return: The created configuration stream.
+    :rtype: str
+    """
+    next_reg_stream = ""
     if config == C_MGMT_OUT:
-        # stream += '0b1100000001001'
-        s = stream + "1100000000001"
+        # Input is not disabled so that there is no additional
+        # transition from 1 to 0
+        next_reg_stream = "1100000000001"
     elif config == C_MGMT_IN:
-        s = stream + "1000000000011"
+        # TODO: Check which value is correct
+        # next_reg_stream = "0010000000011"
+        next_reg_stream = "1000000000011"
     elif config == C_DISABLE:
-        s = stream + "0000000000000"
+        next_reg_stream = "0000000000000"
     elif config == C_ALL_ONES:
-        s = stream + "1111111111111"
+        next_reg_stream = "1111111111111"
     elif config == C_USER_BIDIR_WPU:
-        s = stream + "0100000000000"
+        next_reg_stream = "0100000000000"
     elif config == C_USER_BIDIR_WPD:
-        s = stream + "0110000000000"
+        next_reg_stream = "0110000000000"
     elif config == C_USER_IN_NOPULL:
-        # s = stream + '0010000000010'
-        s = stream + "0010000000011"
+        if htv_type == H_DEPENDENT:
+            # Add another 1 since the input is connected anyway
+            # The added one reduces the number of 1 to 0 transitions
+            next_reg_stream = "0010000000011"
+        else:
+            next_reg_stream = "0010000000010"
     elif config == C_USER_OUT:
-        # s = stream + '0110000000010'
-        # s = stream + '0110000000010'
-        s = stream + "0110000000000"
+        # Input is not disabled here so that there is no additional
+        # transition from 1 to 0
+        next_reg_stream = "1100000000000"
+    elif config == C_MGMT_HIGH_Z_STRONG_0_DISABLE_OUTPUT:
+        next_reg_stream = "1000000000011"
+    elif config == C_MGMT_HIGH_Z_STRONG_0:
+        next_reg_stream = "1000000000001"
+    elif config == C_SPECIAL:
+        next_reg_stream = "1000000000000"
     else:
-        s = stream + "1100000000000"
-    return s
+        next_reg_stream = "1100000000000"
+
+    if htv_type == H_INDEPENDENT:
+        next_reg_stream = next_reg_stream[:-1]
+
+    return stream + next_reg_stream
 
 
-def build_stream_independent(stream, config):
-    s = ""
-    if config == C_MGMT_OUT:
-        # stream += '110000000100'
-        s = stream + "110000000000"
-    elif config == C_MGMT_IN:
-        s = stream + "100000000001"
-    elif config == C_DISABLE:
-        s = stream + "000000000000"
-    elif config == C_ALL_ONES:
-        s = stream + "111111111111"
-    elif config == C_USER_BIDIR_WPU:
-        s = stream + "010000000000"
-    elif config == C_USER_BIDIR_WPD:
-        s = stream + "011000000000"
-    elif config == C_USER_IN_NOPULL:
-        s = stream + "001000000001"
-    elif config == C_USER_OUT:
-        # s = stream + '00110000000010'
-        # s = stream + '011000000001'
-        s = stream + "011000000000"
-    else:
-        s = stream + "110000000000"
-    return s
+def compensate_dependent_htv(stream: str, bpos: int) -> str:
+    """Compensate for one dependent hold time violation (DHTV).
 
+    This is done by replacing the 0 of a 1 to 0 transition by a 1.
+    The reason is that the last 1 before a transition will get lost
+    when there is a dependent hold time violation
 
-def build_stream_none(stream, config):
-    s = ""
-    if config == C_MGMT_OUT:
-        # stream += '1100000001001'
-        s = stream + "1100000000001"
-    elif config == C_MGMT_IN:
-        s = stream + "1000000000011"
-    elif config == C_DISABLE:
-        s = stream + "0000000000000"
-    elif config == C_ALL_ONES:
-        s = stream + "1111111111111"
-    elif config == C_USER_BIDIR_WPU:
-        s = stream + "0100000000000"
-    elif config == C_USER_BIDIR_WPD:
-        s = stream + "0110000000000"
-    elif config == C_USER_IN_NOPULL:
-        s = stream + "0010000000010"
-    elif config == C_USER_OUT:
-        # s = stream + '0110000000010'
-        s = stream + "0110000000000"
-        # s = stream + '0110000000010'
-    else:
-        s = stream + "1100000000000"
-    return s
-
-
-def build_stream_special(stream, config):
-    s = ""
-    s = stream + str(config)
-    return s
-
-
-def correct_dd_holds(stream, bpos):
-    # for x in reversed(range(1,bpos)):
+    :param stream: The stream in which to compensate for a DHTV.
+    :type stream: string
+    :param bpos: The bit position until which to compensate for a DHTV.
+    :type bpos: int
+    :return: The resulting configuration stream after compensating for the DHTV.
+    :rtype: str
+    """
     skip = False
     bits = list(stream)
-    for x in range(1, bpos):
-        if bits[x] == "0" and bits[x - 1] == "1" and not skip:
-            bits[x] = "1"
+    for current_bit_pos in range(1, bpos):
+        # Replace a 0 by a one after a transition from 1 to 0
+        # Since the replacement creates a potential new transition,
+        # the next bit after a replacement will be skipped and not replaced
+        if (
+            bits[current_bit_pos] == "0"
+            and bits[current_bit_pos - 1] == "1"
+            and not skip
+        ):
+            bits[current_bit_pos] = "1"
             skip = True
         else:
             skip = False
-
     return "".join(bits)
 
 
-# ------------------------------------------
-clock = 1
+def build_streams(
+    config_l: List[int],
+    config_h: List[int],
+    violations_l: List[int],
+    violations_h: List[int],
+) -> Tuple[str, str]:
+    """Build the configuration streams for the high and the low chain.
 
-# iterate through each IO in reverse order (e.g. IO[30] to IO[37])
-for k in reversed(range(NUM_IO)):
+    :param config_l: Intended configuration for the low chain.
+    :type config_l: List[int]
+    :param config_h: Intended configuration for the high chain.
+    :type config_l: List[int]
+    :param violations_l: The violation types of the low chain.
+    :type violations_l: List[int]
+    :param violations_l: The violation types of the high chain.
+    :type violations_h: List[int]
+    :return: The high and low configuration streams that were built.
+    :rtype: (str, str)
+    """
+    stream_l = ""
+    stream_h = ""
+    for k in reversed(range(NUM_IO)):
+        stream_l = add_config_to_stream(stream_l, config_l[k], violations_l[k])
+        stream_h = add_config_to_stream(stream_h, config_h[k], violations_h[k])
+    return stream_l, stream_h
 
-    # build upper IO stream
-    if gpio_h[k][1] == H_DEPENDENT:
-        stream_h = build_stream_dependent(stream_h, config_h[k])
-    elif gpio_h[k][1] == H_INDEPENDENT:
-        stream_h = build_stream_independent(stream_h, config_h[k])
-    elif gpio_h[k][1] == H_SPECIAL:
-        stream_h = build_stream_special(stream_h, config_h[k])
-    else:
-        stream_h = build_stream_none(stream_h, config_h[k])
 
-    # build lower IO stream
-    if gpio_l[k][1] == H_DEPENDENT:
-        stream_l = build_stream_dependent(stream_l, config_l[k])
-    elif gpio_l[k][1] == H_INDEPENDENT:
-        stream_l = build_stream_independent(stream_l, config_l[k])
-    elif gpio_l[k][1] == H_SPECIAL:
-        stream_l = build_stream_special(stream_l, config_l[k])
-    elif gpio_l[k][1] == H_NONE:
-        stream_l = stream_l + "1000000000000"
-    else:
-        stream_l = build_stream_none(stream_l, config_l[k])
+def extend_stream_to_n_bits(stream: str, n_bits: int) -> str:
+    """Extend a stream to the given amount of bits by prepending a zero.
 
-n_bits = max(len(stream_h), len(stream_l))
-while len(stream_h) < n_bits:
-    stream_h = "0" + stream_h
-while len(stream_l) < n_bits:
-    stream_l = "0" + stream_l
+    :param stream: The stream to be extended.
+    :type stream: str
+    :param n_bits: The number of bits to which the stream will be extended.
+    :type n_bits: int
+    :return: The extended stream.
+    :rtype: str
+    """
+    while len(stream) < n_bits:
+        stream = "0" + stream
+    return stream
 
-bpos_h = len(stream_h)
-bpos_l = len(stream_l)
-# for k in reversed(range(NUM_IO)):
-# iterate from IO 37 to 35
-for k in range(NUM_IO):
 
-    if gpio_h[k][1] == H_DEPENDENT:
-        stream_h = correct_dd_holds(stream_h, bpos_h)
+def compensate_hold_time_violations(
+    bpos: int, stream: str, violation_types: List[int]
+) -> str:
+    """Compensate for the hold time violations (HTVs) for every IO.
 
-    if gpio_l[k][1] == H_DEPENDENT:
-        stream_l = correct_dd_holds(stream_l, bpos_l)
+    :param bpos: The bit position until which to compensate for dependent hold
+    time violations (DHTVs).
+    :type bpos: int
+    :param stream: The stream to be compensated for HTVs.
+    :type stream: str
+    :param violations_types: A list of the types of hold time violations.
+    :type violations_types: List[List[Union[str, int]]]
+    :return: The stream which is compensated for all HTVs.
+    :rtype: str
+    """
+    for gpio in range(NUM_IO):
+        if violation_types[gpio] == H_DEPENDENT:
+            stream = compensate_dependent_htv(stream, bpos)
 
-    if gpio_h[k][1] == H_INDEPENDENT:
-        bpos_h -= 12
-    elif gpio_h[k][1] == H_SPECIAL:
-        bpos_h -= len(config_h)
-    else:
-        bpos_h -= 13
+        if violation_types[gpio] == H_INDEPENDENT:
+            bpos -= NUM_CONFIG_BITS - 1
+        else:
+            bpos -= NUM_CONFIG_BITS
+    return stream
 
-    if gpio_l[k][1] == H_INDEPENDENT:
-        bpos_l -= 12
-    elif gpio_l[k][1] == H_SPECIAL:
-        bpos_l -= len(config_l)
-    else:
-        bpos_l -= 13
 
-for k in range(n_bits):
-    value = (int(stream_l[k]) << 5) + (int(stream_h[k]) << 6)
-    config_stream.append(0x06 + value)
-    # config_stream.append(0x16 + value)
+def print_streams(stream_l: str, stream_h: str) -> None:
+    """Print the streams for the low and the high IO chain.
 
-config_stream += [0x0] * (247 - len(config_stream))
-n_bits = len(config_stream)
+    :param stream_l: The configuration stream for the low IO chain.
+    :type stream_l:
+    :param stream_h: The configuration stream for the high IO chain.
+    :type stream_h:
+    """
+    print("stream_l:")
+    print(stream_l)
+    print()
+    print("stream_h:")
+    print(stream_h)
+    print()
 
-#
-#  create output files
-#
-print(f"Creating config data for part {part} using {voltage}V.")
-print("stream_h   = " + stream_h)
-print("stream_l   = " + stream_l)
-print("n_bits = {}".format(n_bits))
 
-f = open("gpio_config_data.py", "w")
-f.write("\n")
-f.write("config_data_h = '" + stream_h + "'\n")
-f.write("config_data_l = '" + stream_l + "'\n")
-f.close()
+def add_file_header(f: TextIO, comment_sequence: str) -> None:
+    """Add a header to a given file.
 
-f = open("gpio_config_data.h", "w")
-f.write("\n")
+    :param f: The file to which the header is added.
+    :type TextIO:
+    :param comment_sequence: The comment sequence to use in the file.
+    :type comment_sequence: str
 
-f.write("char config_stream[] = { ")
-f.write("0x{:02x}".format(n_bits))
-for x in config_stream:
-    f.write(", 0x{:02x}".format(x))
-f.write(" };\n")
+    """
+    f.write(
+        f"{comment_sequence} Generated by gpio_config_builder.py for part"
+        + f" {part} and voltage {voltage}V.\n"
+    )
+    f.write(f"{comment_sequence} Time: {datetime.datetime.now()}\n")
+    f.write("\n")
 
-f.close()
+
+def create_python_config_data_file(stream_l: str, stream_h: str) -> None:
+    """Create a python file containing the configuration data.
+
+    :param stream_l: The configuration stream for the low IO chain.
+    :type stream_l:
+    :param stream_h: The configuration stream for the high IO chain.
+    :type stream_h:
+    """
+    f = open("gpio_config_data.py", "w")
+    add_file_header(f, "#")
+    f.write("config_data_h = '" + stream_h + "'\n")
+    f.write("config_data_l = '" + stream_l + "'\n")
+    f.close()
+
+
+def create_config_data_header_file(config_stream: List[int], n_bits: int) -> None:
+    """Create a C header file containing the configuration data.
+
+    :param config_stream: The configuration stream containing the configuration
+    data.
+    :type config_stream: List[int]
+    :param n_bits: The number of bits in the configuration stream.
+    :type n_bits: int
+
+    :param n_bits:
+    :type n_bits: int
+    """
+    f = open("gpio_config_data.h", "w")
+    add_file_header(f, "//")
+
+    f.write("char config_stream[] = {\n")
+    f.write("    0x{:02x}".format(n_bits))
+    for bytenum, byte in enumerate(config_stream):
+        # Add a newline after every 12th byte, considering that the first byte
+        # is already added
+        if ((bytenum + 1) % 12) == 0:
+            f.write(",\n    0x{:02x}".format(byte))
+        else:
+            f.write(", 0x{:02x}".format(byte))
+
+    f.write("};\n")
+    f.close()
+
+
+def build_config_byte_stream(stream_l: str, stream_h: str, n_bits: int) -> List[int]:
+    """Build the config stream as a byte stream
+
+    :param n_bits: The number of bits the bit config stream has.
+    :type n_bits:
+    :return: The built byte config stream.
+    :rtype: List[int]
+    """
+    config_stream = []
+    for bit_pos in range(n_bits):
+        value = (int(stream_l[bit_pos]) << 5) + (int(stream_h[bit_pos]) << 6)
+        config_stream.append(0x06 + value)
+
+    # Add zeros at the end of the stream to compensate for the IHTVs
+    config_stream += [0x00] * (CONFIG_STREAM_TARGET_LEN - n_bits)
+    return config_stream
+
+
+if __name__ == "__main__":
+    print(f"Creating config data for part {part} using {voltage}V.")
+
+    # Extract the violations
+    violations_l = [violation[1] for violation in gpio_l]
+    violations_h = [violation[1] for violation in gpio_h]
+
+    stream_l, stream_h = build_streams(config_l, config_h, violations_l, violations_h)
+
+    longest_stream_len = max(len(stream_h), len(stream_l))
+    stream_l = extend_stream_to_n_bits(stream_l, longest_stream_len)
+    stream_h = extend_stream_to_n_bits(stream_h, longest_stream_len)
+
+    longest_stream_len = max(len(stream_h), len(stream_l))
+
+    bpos_h = len(stream_h)
+    bpos_l = len(stream_l)
+
+    stream_l = compensate_hold_time_violations(bpos_l, stream_l, violations_l)
+    stream_h = compensate_hold_time_violations(bpos_h, stream_h, violations_h)
+
+    config_stream = build_config_byte_stream(stream_l, stream_h, longest_stream_len)
+    len_config_stream = len(config_stream)
+
+    create_python_config_data_file(stream_l, stream_h)
+    create_config_data_header_file(config_stream, len_config_stream)
+    print(f"Length of config stream: {len_config_stream}")
